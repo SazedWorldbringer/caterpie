e237cb63454b11a5644938410d913cf261d93a88
Atharva Pardeshi <atharva.exe@gmail.com>
2025-11-19 23:05:08 +0530

    feat: read cli arguments

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("Error: no website provided")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("Error: too many arguments provided")
+		os.Exit(1)
+	}
+
+	// exactly 1 argument, base url
+	BASE_URL := os.Args[1]
+	fmt.Printf("Starting crawl of: %s\n", BASE_URL)
 }
