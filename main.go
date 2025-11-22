117a1aab9893beeb051e8db7e21a3dcd1eaee5a3
Atharva Pardeshi <atharva.exe@gmail.com>
2025-11-22 17:22:22 +0530

    feat: make concurrent!

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
+
+type config struct {
+	pages              map[string]PageData
+	baseURL            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -21,15 +30,43 @@ func main() {
 	}
 
 	// exactly 1 argument, base url
-	BASE_URL := os.Args[1]
-	fmt.Printf("Starting crawl of: %s\n", BASE_URL)
+	rawBase := os.Args[1]
+	fmt.Printf("Starting crawl of: %s\n", rawBase)
 
-	pages := map[string]int{}
+	parsedBase, err := url.Parse(rawBase)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	crawlPage(BASE_URL, BASE_URL, pages)
+	cfg := &config{
+		pages:              make(map[string]PageData),
+		baseURL:            parsedBase,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, 10),
+		wg:                 &sync.WaitGroup{},
+	}
 
-	for page, value := range pages {
-		fmt.Printf("%s - %d\n", page, value)
+	cfg.wg.Add(1)
+	go cfg.crawlPage(rawBase)
+	cfg.wg.Wait()
+
+	cfg.mu.Lock()
+	finalPages := cfg.pages
+	cfg.mu.Unlock()
+
+	for _, page := range finalPages {
+		fmt.Println("URL:", page.URL)
+		fmt.Println("H1:", page.H1)
+		fmt.Println("First Paragraph:", page.FirstParagraph)
+		fmt.Println("Outgoing links:")
+		for i, url := range page.OutgoingLinks {
+			fmt.Println(i, "-", url)
+		}
+		fmt.Println("ImageURLs:")
+		for i, url := range page.ImageURLs {
+			fmt.Println(i, "-", url)
+		}
+		fmt.Println("---")
 	}
 }
 
@@ -86,48 +123,67 @@ func getHTML(rawURL string) (string, error) {
 	return string(body), nil
 }
 
-func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+// func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+func (cfg *config) crawlPage(rawCurrentURL string) {
+	// signal done
+	defer cfg.wg.Done()
+
 	// check if rawCurrentURL is on the same domain as rawBaseURL
 	// return if not
-	baseURL, err := url.Parse(rawBaseURL)
-	if err != nil {
-		fmt.Println(err)
-	}
 	currentURL, err := url.Parse(rawCurrentURL)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	if strings.Compare(baseURL.Hostname(), currentURL.Hostname()) != 0 {
+	if strings.Compare(cfg.baseURL.Hostname(), currentURL.Hostname()) != 0 {
 		return
 	}
 
 	// check if normalizeURL(rawCurrentURL) is in pages
-	// if yes, increment pages[normalizeURL(rawCurrentURL)]
-	// if no, make pages[normalizeURL(rawCurrentURL)] = 1
-	currentURLstr, err := normalizeURL(currentURL.String())
+	normalized, err := normalizeURL(currentURL.String())
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	_, ok := pages[currentURLstr]
-	if ok {
-		pages[currentURLstr]++
+	cfg.mu.Lock()
+	if _, exists := cfg.pages[normalized]; exists {
+		cfg.mu.Unlock()
 		return
-	} else {
-		pages[currentURLstr] = 1
+	}
+	cfg.pages[normalized] = PageData{URL: currentURL.String()}
+	cfg.mu.Unlock()
+
+	// fmt.Println("current: ", normalized)
+
+	// get html for currentURL and store html in pages[currentURLstr]
+	// limit concurrent access
+	cfg.concurrencyControl <- struct{}{}
+	html, err := getHTML(currentURL.String())
+	<-cfg.concurrencyControl
+	// if error in getting html, store final page data and return
+	if err != nil {
+		cfg.mu.Lock()
+		cfg.pages[normalized] = PageData{
+			URL:            currentURL.String(),
+			H1:             "",
+			FirstParagraph: "",
+		}
+		cfg.mu.Unlock()
+		fmt.Println(err)
+		return
 	}
 
-	// get html for rawCurrentURL and print
-	html, err := getHTML(currentURL.String())
-	if err != nil {
-		fmt.Println(err)
-	}
+	// parse page
+	pageData := extractPageData(html, currentURL)
+
+	cfg.mu.Lock()
+	cfg.pages[normalized] = pageData
+	cfg.mu.Unlock()
 
 	// get all outgoing links from current html
-	outgoingLinks := extractPageData(html, baseURL.String()).OutgoingLinks
-	for _, link := range outgoingLinks {
+	for _, link := range pageData.OutgoingLinks {
 		// recursively crawl each outgoing link
-		crawlPage(rawBaseURL, link, pages)
+		cfg.wg.Add(1)
+		go cfg.crawlPage(link)
 	}
 }
